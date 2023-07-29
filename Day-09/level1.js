5b98820de22cc100d44e08fd02709931cb279aad
NutanMutakekar <138031482+NutanMutakekar@users.noreply.github.com>
2023-07-29 07:37:06 +0530

    Update level1.js

--- Day-09/level1.js
+++ Day-09/level1.js
@@ -12,6 +12,86 @@ const products = [
   { product: 'tea', price: '' },
 ]
 
+//1 Explain the difference between forEach, map, filter, and reduce.
+      // forEach
+      // forEach: Iterate an array elements. We use forEach only with arrays. It takes a callback function with elements, index parameter and array itself. The index and the array optional.
+      // map
+      // map: Iterate an array elements and modify the array elements. It takes a callback function with elements, index , array parameter and return a new array.
+      // filter
+      // Filter: Filter out items which full fill filtering conditions and return a new array.
+      // reduce
+      // reduce: Reduce takes a callback function. The call back function takes accumulator, current, and optional initial value as a parameter and returns a single value. It is a good practice to define an initial value for the accumulator value. If we do not specify this parameter, by default accumulator will get array first value. If our array is an empty array, then Javascript will throw an error.
+
+      // 3 Use forEach to console.log each country in the countries array.
+      countries.forEach((country) => {
+        console.log(country);
+      });
+
+      // 4 Use forEach to console.log each name in the names array.
+      names.forEach((name) => {
+        console.log(name);
+      });
+
+      //5 Use forEach to console.log each number in the numbers array.
+      numbers.forEach((num) => {
+        console.log(num);
+      });
+
+      //  6 Use map to create a new array by changing each country to uppercase in the countries array.
+
+      let uppercaseCountries = countries.map((a) => a.toUpperCase());
+      console.log(uppercaseCountries);
+
+      //  7 Use map to create an array of countries length from countries array.
+
+      let countriesLength = countries.map((a) => a.length);
+      console.log(countriesLength);
+      
+      //8 Use map to create a new array by changing each number to square in the numbers array
+      let square=numbers.map((num)=>{
+return num**2
+      })
+      console.log(square)
+
+      //9 Use map to change to each name to uppercase in the names array
+      let uppercaseNames = names.map((name) => name.toUpperCase());
+      console.log(uppercaseNames);
+
+    // 10  Use map to map the products array to its corresponding prices.
+    let productPrices=products.map((product)=>{
+        return product.price
+    })
+    console.log(productPrices)
+    // for +ve prices in terms of array
+   let positivePrice= productPrices.filter(price=>{
+      return  price>0
+    })
+    console.log(positivePrice)
+
+    // 11 Use filter to filter out countries containing land.
+    let land=countries.filter((country)=>{
+        return country.includes('land')
+    })
+    console.log(land)
+
+    // 12 Use filter to filter out countries having six character.
+    let countriesWith6Chars=countries.filter((country)=>{
+        return country.length>6
+    })
+    console.log(countriesWith6Chars)
+
+    // 13 Use filter to filter out countries containing six letters and more in the country array.
+    let countriesWith6CharsMore=countries.filter((country)=>{
+        return country.length>=6
+    })
+    console.log(countriesWith6CharsMore)
+
+    // 15 Use filter to filter out only prices with values.
+    let positiveValue=products.filter((b)=>{
+    return b.price>0
+})
+console.log(positiveValue)
+
 // 16 Declare a function called getStringLists which takes an array as a parameter and then returns an array only with string items.
 let strArr=['nitu','attu','yashu','renu',24,56]
 
@@ -41,7 +121,7 @@ return acc + 'and' + country
 },'ethopia')
 console.log(concatinatedCountries +'  are European contries')
 
-// 9 Explain the difference between some and every
+// 19 Explain the difference between some and every
 // some
 // some: Check if some of the elements are similar in one aspect. It returns boolean
 
